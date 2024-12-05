01e434917d3d654314cac7e73e90a2eb9236f9bf
Ujjwal Arora <ukarora9@gmail.com>
2024-12-05 17:21:05 +0530

    Initial Commit

---
+++ LearningGoogleSignIn/LearningGoogleSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGoogleSignInApp.swift
+//  LearningGoogleSignIn
+//
+//  Created by Ujjwal Arora on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningGoogleSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
